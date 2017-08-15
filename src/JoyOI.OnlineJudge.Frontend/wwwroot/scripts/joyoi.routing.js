86250461af6056f23095f056f4720eb67400ec9e
あまみや ゆうこ <1@1234.sh>
2017-08-15 23:11:21 +0800

    implement status page

--- src/JoyOI.OnlineJudge.Frontend/wwwroot/scripts/joyoi.routing.js
+++ src/JoyOI.OnlineJudge.Frontend/wwwroot/scripts/joyoi.routing.js
@@ -2,6 +2,7 @@
     '/home': null,
     '/problem/all': null,
     '/problem/:id': { id: '[a-zA-Z0-9-_]{4,128}' },
+    '/judge/all': null,
     '/foo/:id': { id: '[0-9a-zA-Z-]{3,16}' },
     '/bar': null,
     '/404': null
@@ -9,5 +10,6 @@
 
 LazyRouting.SetMirror({
     '/': '/home',
-    '/problem': '/problem/all'
+    '/problem': '/problem/all',
+    '/judge': '/judge/all'
 });
