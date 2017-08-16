2af6f60da000029e77c558ec0f51d70649fc1d45
あまみや ゆうこ <1@1234.sh>
2017-08-16 19:29:58 +0800

    Implement filters for status

--- src/JoyOI.OnlineJudge.Frontend/wwwroot/scripts/joyoi.extend.js
+++ src/JoyOI.OnlineJudge.Frontend/wwwroot/scripts/joyoi.extend.js
@@ -38,7 +38,6 @@ function removeApplicationManager(uid)
 }
 
 $(window).on('mousemove', function (e) {
-    console.log(e);
     if ($(e.target).hasClass('tag-item') || $(e.target).parents().hasClass('tag-item')) {
         var val = $(e.target).attr('data-value');
         $('[data-parent="' + val + '"]').addClass('active');
@@ -50,5 +49,64 @@ $(window).on('mousemove', function (e) {
     if (!$(e.target).parents().hasClass('tag-item') && !$(e.target).hasClass('tag-item') && !$(e.target).parents().hasClass('tag-extend-outer') && !$(e.target).hasClass('tag-extend-outer'))
     {
         $('[data-parent]').removeClass('active');
+    }
+});
+
+var statuses = [
+    { display: 'Accepted', color: 'green', value: 0 },
+    { display: 'Presentation Error', color: 'red', value: 1 },
+    { display: 'Wrong Answer', color: 'red', value: 2 },
+    { display: 'Output Exceeded', color: 'red', value: 3 },
+    { display: 'Time Exceeded', color: 'red', value: 4 },
+    { display: 'Memory Exceeded', color: 'red', value: 5 },
+    { display: 'Runtime Error', color: 'red', value: 6 },
+    { display: 'Compile Error', color: 'orange', value: 7 },
+    { display: 'System Error', color: 'orange', value: 8 },
+    { display: 'Hacked', color: 'red', value: 9 },
+    { display: 'Running', color: 'blue', value: 10 },
+    { display: 'Pending', color: 'blue', value: 11 },
+    { display: 'Hidden', color: 'purple', value: 12 }
+];
+
+var languages = [
+    'C',
+    'C++',
+    'Pascal',
+    'C#',
+    'Python',
+    'Ruby',
+    'JavaScript'
+];
+
+$(window).click(function (e) {
+    var dom = $(e.target);
+    if (dom.parents('.filter-button'))
+        dom = dom.parents('.filter-button');
+    else if (!dom.hasClass('filter-button'))
+        return;
+    var box = dom.next('.filter-outer');
+    if (!box.hasClass('filter-outer'))
+        return;
+    if (box.hasClass('active'))
+    {
+        box.removeClass('active');
+    }
+    else
+    {
+        $('.filter-outer.active').removeClass('active');
+        box.addClass('active');
+        box.css('top', box.parent('th').offset().top + box.parent('th').outerHeight());
+
+        if ($(window).width() >= 768) {
+            if (box.hasClass('submit-filter') || box.hasClass('language-filter')) {
+                box.css('left', box.parent('th').offset().left - box.outerWidth() + box.parent('th').outerWidth());
+            } else {
+                box.css('left', box.parent('th').offset().left);
+            }
+
+            if (box.hasClass('problem-filter')) {
+                box.outerWidth(box.parent('th').outerWidth());
+            }
+        }
     }
 });
