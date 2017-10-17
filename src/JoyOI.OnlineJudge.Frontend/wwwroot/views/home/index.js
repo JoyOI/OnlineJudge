e5540009abe2e169563e4fd6d7c728b74e90aa23
あまみや ゆうこ <1@1234.sh>
2017-10-17 08:40:07 +0800

    Joint home page forum threads

--- src/JoyOI.OnlineJudge.Frontend/wwwroot/views/home/index.js
+++ src/JoyOI.OnlineJudge.Frontend/wwwroot/views/home/index.js
@@ -6,6 +6,19 @@ component.data = function () {
         contests: [
             { title: 'Joy OI Test Round #1', description: 'blablabla' },
             { title: 'Test', description: 'blablabla' },
-        ]
+        ],
+        threads: []
     }
+}
+
+component.created = function () {
+    var self = this;
+    qv.createView('/api/forum/summary', null, 60000)
+        .fetch(x =>
+        {
+            self.threads = x.data;
+        })
+        .catch(err => {
+            app.notification('error', '获取论坛帖子失败', err.responseJSON.msg);
+        });
 }
