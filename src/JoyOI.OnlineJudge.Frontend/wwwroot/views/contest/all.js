676b7f6b36b7bdd11ab3dbae912e3cdf0d91eb70
あまみや ゆうこ <1@1234.sh>
2017-10-27 09:07:56 +0800

    Implement show all contest javascript

--- src/JoyOI.OnlineJudge.Frontend/wwwroot/views/contest/all.js
+++ src/JoyOI.OnlineJudge.Frontend/wwwroot/views/contest/all.js
@@ -1,10 +1,22 @@
-﻿<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title></title>
-</head>
-<body>
+﻿app.title = '比赛';
+app.links = [];
 
-</body>
-</html>
+component.data = function () {
+    return {
+        contests: [
+            { title: 'Joy OI Test Round #1', description: 'blablabla' },
+            { title: 'Test', description: 'blablabla' },
+        ]
+    }
+}
+
+component.created = function () {
+    var self = this;
+    qv.createView('/api/contest/all', null, 60000)
+        .fetch(x => {
+            self.contests = x.data;
+        })
+        .catch(err => {
+            app.notification('error', '获取比赛失败', err.responseJSON.msg);
+        });
+}
