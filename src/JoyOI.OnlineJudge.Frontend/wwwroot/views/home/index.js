7277a2ac0d567a4e8514d418e935eb8f0ba20536
あまみや ゆうこ <1@1234.sh>
2017-11-02 09:09:15 +0800

    joint home page contest list

--- src/JoyOI.OnlineJudge.Frontend/wwwroot/views/home/index.js
+++ src/JoyOI.OnlineJudge.Frontend/wwwroot/views/home/index.js
@@ -3,22 +3,51 @@ app.links = [];
 
 component.data = function () {
     return {
-        contests: [
-            { title: 'Joy OI Test Round #1', description: 'blablabla' },
-            { title: 'Test', description: 'blablabla' },
-        ],
+        contests: [],
         threads: []
     }
 }
 
 component.created = function () {
-    var self = this;
-    qv.createView('/api/forum/summary', null, 60000)
-        .fetch(x =>
-        {
-            self.threads = x.data;
-        })
-        .catch(err => {
-            app.notification('error', '获取论坛帖子失败', err.responseJSON.msg);
-        });
+    this.loadContests();
+    this.loadThreads();
 }
+
+component.methods = {
+    loadThreads: function () {
+        var self = this;
+        qv.createView('/api/forum/summary', null, 60000)
+            .fetch(x => {
+                self.threads = x.data;
+            })
+            .catch(err => {
+                app.notification('error', '获取论坛帖子失败', err.responseJSON.msg);
+            });
+    },
+    loadContests: function () {
+        var self = this;
+        qv.createView('/api/contest/all', {}, 60000)
+            .fetch(x => {
+                var results = clone(x.data.result);
+                results = results.map(y => {
+                    var begin = new Date(y.begin);
+                    var end = new Date(begin.getTime() + parseTimeSpan(y.duration))
+                    if (new Date() < begin) {
+                        y.status = 'Ready';
+                        y.statusClass = 'contest-ready';
+                    } else if (new Date() < end) {
+                        y.status = 'Live';
+                        y.statusClass = 'contest-live';
+                    } else {
+                        y.status = 'Done';
+                        y.statusClass = 'contest-done';
+                    }
+                    return y;
+                });
+                self.contests = results;
+            })
+            .catch(err => {
+                app.notification('error', '获取比赛失败', err.responseJSON.msg);
+            });
+    }
+};
