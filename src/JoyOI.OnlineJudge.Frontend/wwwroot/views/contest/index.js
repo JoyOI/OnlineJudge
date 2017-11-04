b510c01ad50729b29befab89dfaaa93a79da64e8
あまみや ゆうこ <1@1234.sh>
2017-11-04 14:19:03 +0800

    Adding infrastructures for contest detail page

--- src/JoyOI.OnlineJudge.Frontend/wwwroot/views/contest/index.js
+++ src/JoyOI.OnlineJudge.Frontend/wwwroot/views/contest/index.js
@@ -1,1 +1,10 @@
-﻿
+﻿app.title = '比赛';
+app.links = [{ text: '比赛列表', to: '/contest' }];
+
+component.data = function () {
+    return {
+        id: router.history.current.params.id,
+        title: null,
+        body: null
+    };
+};
