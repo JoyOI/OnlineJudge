233816b1b1d4eb2f5ab56a0b095e0e84e526e635
あまみや ゆうこ <1@1234.sh>
2017-11-14 21:24:55 +0800

    Implement LeetCode problem import api

--- src/JoyOI.OnlineJudge.WebApi/Controllers/Management/ProblemController.cs
+++ src/JoyOI.OnlineJudge.WebApi/Controllers/Management/ProblemController.cs
@@ -57,5 +57,49 @@ namespace JoyOI.OnlineJudge.WebApi.Controllers.ManagementServiceCallback
 
             return Result(200, "ok");
         }
+
+        [HttpPost("LeetCode/{id:Guid}")]
+        public async Task<IActionResult> LeetCode(
+            [FromServices] ManagementServiceClient MgmtSvc,
+            Guid id)
+        {
+            var statemachine = await MgmtSvc.GetStateMachineInstanceAsync(id, default(CancellationToken));
+            var actors = statemachine.StartedActors.Where(x => x.Name == "LeetCodePullProblemBodyActor" && x.Outputs.Any(y => y.Name == "problem.json")).ToDictionary(x => "bzoj-" + x.Tag);
+            var ids = actors.Keys.Select(x => x).ToList();
+
+            foreach (var i in ids)
+            {
+                var model = JsonConvert.DeserializeObject<ProblemJson>(Encoding.UTF8.GetString((await ManagementService.GetBlobAsync(actors[i].Outputs.Single(x => x.Name == "problem.json").Id)).Body));
+                var problem = await DB.Problems.SingleOrDefaultAsync(x => x.Id == i);
+                if (problem == null)
+                {
+                    problem = new Problem
+                    {
+                        Title = model.Title,
+                        Id = i,
+                        Body = model.Body,
+                        Source = ProblemSource.LeetCode,
+                        Tags = "按题库:LeetCode",
+                        TimeLimitationPerCaseInMs = model.TimeLimitInMs,
+                        MemoryLimitationPerCaseInByte = model.MemoryLimitInByte,
+                        IsVisiable = true,
+                        Template = JsonConvert.SerializeObject(model.CodeTemplate)
+                    };
+                    DB.Problems.Add(problem);
+                }
+                else
+                {
+                    problem.Title = model.Title;
+                    problem.Body = model.Body;
+                    problem.TimeLimitationPerCaseInMs = model.TimeLimitInMs;
+                    problem.MemoryLimitationPerCaseInByte = model.MemoryLimitInByte;
+                    problem.IsVisiable = true;
+                }
+                await DB.SaveChangesAsync();
+            }
+
+            return Result(200, "ok");
+        }
+
     }
 }
